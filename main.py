9876c5c74d52fffb7db73e1888c9ca473b8ae207
naokodama <loaf.m.wind5420@gmail.com>
2021-02-04 23:53:28 +0900

    create the method to change the tab to the space.

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def delete_crlf_code_from_top(original_text):
     new_text = re.sub(r"^\r|\n\r|\n", r"", original_text)
     return new_text
 
+def change_tab_to_space4(original_text):
+    new_text = re.sub(r"\t", r"    ", original_text)
+    return new_text
+
 def delete_crlf_and_tab(original_text):
     new_text = re.sub(r"\r|\n\r|\n|\t", r"", original_text)
     return new_text
@@ -40,7 +44,6 @@ def delete_font_tag(html_text):
     pattern = re.compile(r"<FONT[\s\S]*?>([\s\S]*?)</FONT>")
     new_text = ""
     if pattern.search(html_text):
-        print("match!")
         new_text = re.sub(pattern, r"\1", html_text)
         new_text = delete_font_tag(new_text)
     else:
@@ -115,6 +118,7 @@ class Application(tk.Frame):
             html_text = delete_crlf_code_from_top(clipboard_text)
             html_text = get_table_item(html_text)
             html_text = delete_font_tag(html_text)
+            html_text = change_tab_to_space4(html_text)
         else:
             html_text = ""
         self.text_area.delete("1.0", tk.END)
